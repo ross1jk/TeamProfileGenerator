cc081760395ee7be19d94587b1a75aa462d80977
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-02-11 18:55:31 -0500

    manager prompts created and pushing to an array

--- index.js
+++ index.js
@@ -1,2 +1,40 @@
-const Employee = require("./lib/Employee"); 
-const employee = new Employee(); 
+const inquirer = require("inquirer"); 
+const fs = require("fs"); 
+
+let team = []; 
+
+function manager(){
+    inquirer.prompt([
+        {
+          type: "input",
+          name: "name",  
+          message: "Provide your name"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Provide your employee ID"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Provide your email"
+        },
+        {
+            type: "input",
+            name: "office",
+            message: "Provide your office number"
+        }
+    ]).then(function (data){
+        const name = data.name;
+        const id = data.id; 
+        const email = data.email; 
+        const office = data.office; 
+        let manager = {name, id, email, office}; 
+        team.push(manager); 
+        console.log(team); 
+    }
+    )
+}
+manager(); 
+
