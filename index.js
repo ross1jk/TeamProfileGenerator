044b6a05676034a1c96250bfc37a6fdf21d33c2e
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-02-11 19:25:25 -0500

    prompts run through for each team member and type and logs into an array

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer"); 
 const fs = require("fs"); 
+const { ENGINE_METHOD_RAND } = require("constants");
 
 let team = []; 
 
@@ -33,7 +34,6 @@ function manager(){
         const office = data.office; 
         let manager = {role, name, id, email, office}; 
         team.push(manager); 
-        console.log(team);
         addTeamMember(); 
     }
     );
@@ -63,8 +63,86 @@ function addTeamMember(){
                         "Intern"
                     ]
                 }
-            ])
+            ]).then(function (data){
+                if (data.employeeType === "Engineer"){
+                    addEngineer();
+                } if (data.employeeType === "Intern"){
+                    addIntern(); 
+                }
+            });
+        } if (data.add === "No"){
+            console.log(team);
         }
-    })
+    }); 
+}; 
+
+function addEngineer(){
+    inquirer.prompt([
+        {
+          type: "input",
+          name: "name",  
+          message: "Provide their name"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Provide their employee ID"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Provide their email"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "Provide their github username"
+        }
+    ]).then(function (data){
+        const role = 'engineer'
+        const name = data.name;
+        const id = data.id; 
+        const email = data.email; 
+        const github = data.github; 
+        let engineer = {role, name, id, email, github}; 
+        team.push(engineer); 
+        addTeamMember(); 
+    }
+    );
 }
+
+function addIntern(){
+    inquirer.prompt([
+        {
+          type: "input",
+          name: "name",  
+          message: "Provide their name"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Provide their employee ID"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Provide their email"
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "Provide the school they attend"
+        }
+    ]).then(function (data){
+        const role = 'intern'
+        const name = data.name;
+        const id = data.id; 
+        const email = data.email; 
+        const school = data.school; 
+        let intern = {role, name, id, email, school}; 
+        team.push(intern); 
+        addTeamMember(); 
+    }
+    );
+}; 
 
