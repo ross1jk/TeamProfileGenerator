ea793e1372f3647141d1a6121d3b8f397e3ab8c3
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-02-12 21:24:35 -0500

    generating html for one card entry, need two

--- generateHTML.js
+++ generateHTML.js
@@ -1,20 +1,45 @@
-// function generateCard(){
-//     for (i = 0; i < team.length; i++){
-//         let type = team[i].office || team[i].github || team[i].school; 
-// let card = `
-// <div class="card">
-//   <h5 class="card-header">${team[i].name}</h5>
-//   <div class="card-body">
-//     <h5>${team[i].role}</h5>
-//     <h6>${team[i].id}</h6>
-//     <h6>${team[i].email}</h6>
-//     <h6>${type}</h6>
-//   </div>
-// </div>
-// ` 
-//   }
+function generateCard(){
+    for (i = 0; i < team.length; i++){        
+        if (team[i].role === "manager"){
+            return `
+            <div class="card">
+              <h5 class="card-header">${team[i].name}</h5>
+              <div class="card-body">
+                <h5>${team[i].role}</h5>
+                <h6>${team[i].id}</h6>
+                <h6>${team[i].email}</h6>
+                <h6>${team[i].office}</h6>
+              </div>
+            </div>
+            ` 
+        } if(team[i].role === "engineer"){
+            return `
+            <div class="card">
+              <h5 class="card-header">${team[i].name}</h5>
+              <div class="card-body">
+                <h5>${team[i].role}</h5>
+                <h6>${team[i].id}</h6>
+                <h6>${team[i].email}</h6>
+                <h6>${team[i].github}</h6>
+              </div>
+            </div>
+            ` 
+         }if (team[i].role === "intern"){
+            return `
+            <div class="card">
+              <h5 class="card-header">${team[i].name}</h5>
+              <div class="card-body">
+                <h5>${team[i].role}</h5>
+                <h6>${team[i].id}</h6>
+                <h6>${team[i].email}</h6>
+                <h6>${team[i].school}</h6>
+              </div>
+            </div>
+          ` 
+        }
+  }
 
-function generateHTML(){
+function generateHTML(data){
     return `
 <!DOCTYPE html>
 <html lang="en">
@@ -23,9 +48,9 @@ function generateHTML(){
       <title>Team Profile</title>
 </head>
 <body> 
-    
+    ${generateCard(data)}
 </body>
 </html>`
 }
 
-module.exports = generateHTML; 
+module.exports = generateHTML;  
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer"); 
 const fs = require("fs"); 
-const generateHTML = require("./generateHTML");
+// const generateHTML = require("./generateHTML");
 const { create } = require("domain");
 
 let team = []; 
@@ -19,11 +19,11 @@ function createTeam(){
         }
     ]).then(function (data){
         if (data.intro === "Manager"){
-            addManager(); 
+            addManager()
         }if (data.intro === "Engineer"){
-            addEngineer();
+            addEngineer()
         }if (data.intro === "Intern"){
-            addIntern(); 
+            addIntern()
         }
     })
 }
@@ -64,7 +64,7 @@ function addTeamMember(){
             });
         } if (data.add === "No"){
             console.log(team);
-            // generateHTML();
+            writeToFile(); 
         }
     }); 
 }; 
@@ -175,15 +175,66 @@ function addIntern(){
 }; 
 
 createTeam();
-// function writeToFile(data) {
-//     fs.writeFile("teamprofile.html", generateHTML(data) , (err) =>
-//     err ? console.log(err) : console.log('Your Team Profile Application has been generated successfully!'))
-// }
 
-// function init(){
-//     manager().then((data) => {
-//         writeToFile(data); 
-//     })
-// }; 
+function generateCard(){
+    for (i = 0; i < team.length; i++){        
+        if (team[i].role === "manager"){
+            return `
+            <div class="card">
+              <h5 class="card-header">${team[i].name}</h5>
+              <div class="card-body">
+                <h5>${team[i].role}</h5>
+                <h6>${team[i].id}</h6>
+                <h6>${team[i].email}</h6>
+                <h6>${team[i].office}</h6>
+              </div>
+            </div>
+            ` 
+        } if(team[i].role === "engineer"){
+            return `
+            <div class="card">
+              <h5 class="card-header">${team[i].name}</h5>
+              <div class="card-body">
+                <h5>${team[i].role}</h5>
+                <h6>${team[i].id}</h6>
+                <h6>${team[i].email}</h6>
+                <h6>${team[i].github}</h6>
+              </div>
+            </div>
+            ` 
+         }if (team[i].role === "intern"){
+            return `
+            <div class="card">
+              <h5 class="card-header">${team[i].name}</h5>
+              <div class="card-body">
+                <h5>${team[i].role}</h5>
+                <h6>${team[i].id}</h6>
+                <h6>${team[i].email}</h6>
+                <h6>${team[i].school}</h6>
+              </div>
+            </div>
+          ` 
+        }
+  }
+}
 
-// init(); 
+function generateHTML(data){
+    return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <title>Team Profile</title>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<body> 
+    ${generateCard(data)}
+</body>
+</html>`
+}
+
+
+function writeToFile(data) {
+    fs.writeFile("teamprofile.html", generateHTML(data) , (err) =>
+    err ? console.log(err) : console.log('Your Team Profile Application has been generated successfully!'))
+}
