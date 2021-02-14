9ee4427f32ef0371030b7a35bec601e1a298706e
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-02-14 18:41:36 -0500

    2 manager tests passing

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -2,19 +2,29 @@ const Employee = require("../lib/Employee");
 const Manager = require("../lib/Manager.js");
 
 describe("Manager", () => {
-    // describe intialization 
     // test to ensure they are set from the super like in employee
-    // but do not need to test if string etc 
-    // will need to double check get role probs 
-})
+    describe("Intialization", () => {
+        it("should create an object with a name, id, and email if provided valid arguments", () => {
+            const manager = new Manager("Jacqueline", 44, "jr@gmail.com", "a123");
+            expect(manager.name).toEqual("Jacqueline");
+            expect(manager.id).toEqual(44);
+            expect(manager.email).toEqual("jr@gmail.com");
+            expect(manager.officeNumber).toEqual("a123");
+        });
+    });
+});
 // will have to remove some stuff watch the video 
 describe("getOfficeNumber", () => {
     it("should run the function to get the office number", () => {
-    const manager = new Manager("jac", 1, "jr@gmail.com", "a123"); 
-    const managerSpy = jest.spyOn(manager, "getOfficeNumber"); 
-    manager.getOfficeNumber(); 
-    expect(managerSpy).toBeCalled(); 
-    managerSpy.mockImplementation(() => { return "a123"}); 
-    expect(managerSpy()).toEqual("a123"); 
+        const manager = new Manager("Jacqueline", 44, "jr@gmail.com", "a123");
+        const managerSpy = jest.spyOn(manager, "getOfficeNumber");
+        manager.getOfficeNumber();
+        expect(managerSpy).toBeCalled();
+        managerSpy.mockImplementation(() => { return "a123" });
+        expect(managerSpy()).toEqual("a123");
     });
 }); 
+
+describe("getRole")
+
+  // will need to double check get role probs 
